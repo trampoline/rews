17f3b176147458bf14b63ae36cb04b5fd86ef99a
mccraig mccraig of the clan mccraig <mccraigmccraig@googlemail.com>
2011-03-15 17:32:36 +0000

    client_spec

--- lib/rews.rb
+++ lib/rews.rb
@@ -19,37 +19,5 @@ require 'rews/sort_order'
 require 'rews/view'
 require 'rews/folder'
 require 'rews/item'
+require 'rews/client'
 
-module Rews
-  class Client
-    attr_reader :client
-    attr_accessor :logdev
-
-    # Rews::Client.new('https://exchange.foo.com/EWS/Exchange.asmx', :ntlm, 'DOMAIN\\user', 'password')
-    # Rews::Client.new('https://exchange.foo.com/EWS/Exchange.asmx', :basic, 'DOMAIN\\user', 'password')
-    def initialize(endpoint, auth_type, user, password)
-      @client = Savon::Client.new do
-        wsdl.endpoint = endpoint
-        wsdl.namespace = SCHEMA_MESSAGES
-        
-        http.auth.ssl.verify_mode = :none
-        http.auth.send(auth_type, user, password)
-      end
-    end
-
-    # client.distinguished_folder_id('inbox')
-    # client.distinguished_folder_id('inbox', 'foo@bar.com') # to get a folder from another mailbox
-    def distinguished_folder_id(id, mailbox_email=nil)
-      Folder::DistinguishedFolderId.new(client, id, mailbox_email)
-    end
-
-    def log
-      yield logger if @logdev
-    end
-
-    def logger
-      return @logger if @logger
-      @logger = Logger.new(@logdev) if @logdev
-    end
-  end
-end
---
+++ spec/rews/client_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+module Rews
+  describe Client do
+    it "should create new DistinguishedFolderIds for arbitrary mailboxes" do
+      client = Client.new("https://foo/EWS/Exchange.asmx", :ntlm, "EXCHDOM\\foo", "password")
+
+      mock(Folder::DistinguishedFolderId).new(client.client, 'inbox', 'foo@bar.com')
+
+      client.distinguished_folder_id('inbox', 'foo@bar.com')
+    end
+
+    it "should create new DistinguishedFolderIds for the default mailbox" do
+      client = Client.new("https://foo/EWS/Exchange.asmx", :ntlm, "EXCHDOM\\foo", "password")
+
+      mock(Folder::DistinguishedFolderId).new(client.client, 'inbox', nil)
+
+      client.distinguished_folder_id('inbox')
+    end
+  end
+end
